3e0ac8f1b4dddcc8e92006439df385e692b5b7bf
Joshua Read <joshua.gray.read@gmail.com>
2018-08-08 11:39:53 +0200

    Pushing Styles & templates

---
+++ footer.php
@@ -0,0 +1,20 @@
+  	  </div>
+	</div>
+  </div>
+</div>
+</body>
+
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/js/jquery.min.js"></script>
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/revolution/js/jquery.themepunch.tools.min.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/revolution/js/jquery.themepunch.revolution.min.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/revolution/js/extensions/revolution.extension.slideanims.min.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/revolution/js/extensions/revolution.extension.navigation.min.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/revolution/js/extensions/revolution.extension.carousel.min.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/revolution/js/extensions/revolution.extension.video.min.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/revolution/js/extensions/revolution.extension.kenburn.min.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/js/plugins.js"></script> 
+<script type="text/javascript" src="https://drjoshuaread.com/app/aprovision/style/js/scripts.js"></script>
+</body>
+</html>
