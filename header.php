ceb4e88adcb591969fc64d0d10bb29773f0fc574
Joshua Read <joshua.gray.read@gmail.com>
2018-08-08 11:40:04 +0200

    Pushing Styles & templates

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="text" content="">
+<meta name="author" content="">
+<link rel="shortcut icon" href="https://drjoshuaread.com/app/aprovision/style/images/favicon.png">
+<title>AppCentrix Auto Provisioner Tool</title>
+<link rel="stylesheet" type="text/css" href="https://drjoshuaread.com/app/aprovision/style/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://drjoshuaread.com/app/aprovision/style/css/plugins.css">
+<link rel="stylesheet" type="text/css" href="https://drjoshuaread.com/app/aprovision/style/revolution/css/settings.css">
+<link rel="stylesheet" type="text/css" href="https://drjoshuaread.com/app/aprovision/style/revolution/css/layers.css">
+<link rel="stylesheet" type="text/css" href="https://drjoshuaread.com/app/aprovision/style/revolution/css/navigation.css">
+<link rel="stylesheet" type="text/css" href="https://drjoshuaread.com/app/aprovision/style/type/icons.css">
+<link rel="stylesheet" type="text/css" href="https://drjoshuaread.com/app/aprovision/style.css">
+<link rel="stylesheet" type="text/css" href="https://drjoshuaread.com/app/aprovision/style/css/color/orange.css">
+<style type="text/css">
+	#noBmargin {margin-bottom: 0px;}
+	#customMargins {margin: 0px 14px;}
+	.panel-group-lined .panel-title > a {padding: 20px 0px;}
+	.panel-group .panel-heading .panel-title {font-weight: 400;}
+	.panel-group-lined .panel-body {
+	    padding: 28px 0;
+	    border: 0;
+	    margin-bottom: 50px;
+	}
+</style>
+<link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body class="index-page">
+  <div class="wrapper light-wrapper">
+    <div class="container inner">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">	
